7fda6d9387e4c51d3229ce21896514618d32aa77
Ananya-Arun <ananyarun17@gmail.com>
2025-02-14 23:54:15 +0530

    Updated code and set port to 3000

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
- // Check this path
+
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
@@ -9,10 +9,11 @@ import Skills from "./pages/Skills";
 
 function App() {
   return (
-    <Router>
+    // Set the basename for routing when deployed on GitHub Pages (subdirectory path)
+    <Router basename="/my-portfolio">
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} /> {/* Home is the default route */}
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/skills" element={<Skills />} />
